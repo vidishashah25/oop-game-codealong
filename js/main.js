af47830654e29ae6735b84de21cbd08843d8da3d
Vidisha Shah <shahvidisha1996@gmail.com>
2023-10-26 12:57:38 +0200

    implement functionality to move player.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,45 @@
-console.log("js loaded....")
+class Player{
+    constructor(){
+
+        //initialize properties
+        this.positionX = 0;
+        this.positionY = 0;
+        this.height = 20; 
+        this.width = 20;
+
+        //dom manipulation to reflect initial values (size, position)
+        this.playerElm = document.getElementById("player");
+        this.playerElm.style.width = this.width + "vw";
+        this.playerElm.style.height = this.height + "vh";
+        this.playerElm.style.bottom = this.positionY + "vh";
+        this.playerElm.style.left = this.positionX + "vw";
+
+    }
+    moveLeft(){
+        this.positionX--;       
+        this.playerElm.style.left = this.positionX + "vw";
+      
+    }
+    moveRight(){
+        this.positionX++;    
+        this.playerElm.style.left = this.positionX + "vw";
+     
+    }
+}
+
+const player = new Player();
+
+
+document.addEventListener("keydown", (e) => {
+    switch (e.code) {
+        case "ArrowLeft":
+            console.log("pressed left");
+            player.moveLeft();
+            break;
+        case "ArrowRight":
+            console.log("pressed right");
+            player.moveRight();
+            break;
+       
+    }
+});
